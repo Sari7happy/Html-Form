c45b06f497ca2d21dbf4ab69ea9fc7bc0d581a43
sari7happy <sari7happy@gmail.com>
2022-09-18 15:55:34 +0900

    ”エラー項目追加”

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
 $(function() {
-    $('#reservation').datepicker();
-});
-// datepickerは日付選ぶ時に使う/
+  $('#reservation').datepicker();
+  // datepickerは日付選ぶ時に使う/
+  $('.lesson').on('change',function() {
+      let lesson= $('input[name=your-lesson]:checked').val();
+      if(lesson =='online') {
+          $('.note-online').css('display','block');
+          $('.note-venue').css('display','none');
+      } else if(lesson=='venue') {
+          $('note-online').css('display','none');
+          $('note-venue').css('display','block');
+      }
+  });
+  
+  $('#submit-button').on('click',function(){
+          $('#name-error').text("");
+          $('#tel-error').text("");
+          $('#reservation-error').text("");
+          $('#lesson-error').text("");
+
+      if($('input[name="your-name"]').val() =="") {
+          $("#name-error").text("名前は必須項目です。");
+      }
+      if(
+          ($('input[name="your-tel1]').val()=="") ||
+          ($('input[name="your-tel2]').val()=="") ||
+          ($('input[name="your-tel3]').val()=="")
+      ) {
+          $("#tel-error").text("電話番号は必須項目です。");
+      }
+
+      if($('input[name="your-reservation"]').val() ==""){
+          $("#reservation-error").text("予約日は必須項目です。");
+      }
+  });
+});   
